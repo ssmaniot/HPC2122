88e0ecb06e74ed0913a96df415271712585788d3
Sebastiano Smaniotto <ssmaniot@gmail.com>
2022-08-08 00:01:59 +0200

    make_data.cpp now generates csv with random samples from multivariate normal distributions.

--- make_data.cpp
+++ make_data.cpp
@@ -1,34 +1,47 @@
+#include <iomanip>
 #include <iostream>
-#include <fstream>
+#include <random>
 
-int main(int argc, char *argv[])
-{
-    double data[4] = {9.5, -3.4, 1.0, 2.1};
-    int i;
+#include "MatrixOperations.hpp"
+#include "MultivariateNormalDistribution.hpp"
 
-    std::ofstream out("test.bin", std::ios::out | std::ios::binary);
-    if (!out)
-    {
-        std::cerr << "Cannot open file.\n";
-        return 1;
+int main(int argc, char *argv[]) {
+  constexpr size_t p = 7;
+  constexpr size_t min_n = 3;
+  constexpr size_t max_n = 10;
+  constexpr size_t groups = 3;
+  MappedData::Linalg::Matrix<double> m{1, p};
+  size_t i, j, n, g;
+
+  std::random_device rd{};
+  std::mt19937 mt{rd()};
+  std::normal_distribution<double> ndist{};
+  std::uniform_int_distribution<size_t> udist{min_n, max_n};
+
+  std::cout << "group,";
+  for (i = 0; i < p; ++i) {
+    std::cout << 'x' << i;
+    if (i < p - 1) {
+      std::cout << ',';
     }
+  }
+  std::cout << '\n';
+  for (g = 0; g < groups; ++g) {
+    for (i = 0; i < p; ++i) {
+      m(0, i) = 3 * ndist(mt);
+    }
+    n = udist(mt);
+    for (i = 0; i < n; ++i) {
+      std::cout << g << ',';
+      for (j = 0; j < p; ++j) {
+        std::cout << (m(0, j) + ndist(mt));
+        if (j < p - 1) {
+          std::cout << ',';
+        }
+      }
+      std::cout << '\n';
+    }
+  }
 
-    out.write((char *)&data, sizeof(data));
-    out.close();
-
-    for (i = 0; i < 4; ++i)
-        data[i] = 0;
-
-    std::ifstream in("test.bin", std::ios::in | std::ios::binary);
-    in.read((char *)&data, sizeof(data));
-
-    // see how many bytes have been read
-    std::cout << in.gcount() << " bytes read\n";
-
-    for (i = 0; i < 4; ++i)
-        std::cout << data[i] << ' ';
-    std::cout << '\n';
-
-    in.close();
-    return 0;
+  return 0;
 }
