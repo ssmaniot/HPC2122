08286d12edd0bbf817fa4ffb39036768afd81de9
Sebastiano Smaniotto <ssmaniot@gmail.com>
2022-08-08 00:00:57 +0200

    Updated compile.sh script to avoid compiling src/slink.cpp when targetting main.cpp

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ g++ -std=c++23 -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-priv
        	-Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast \
 	-Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel         \
 	-Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused                                              \
-	-o main src/*.cpp -I include/ -fopenmp
+	-o main src/*[^k].cpp -I include/ -fopenmp
