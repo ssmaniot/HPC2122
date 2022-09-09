52bebcd0ce44c2591ad5554692ad37d3aa5aba0c
Sebastiano Smaniotto <ssmaniot@gmail.com>
2022-09-09 07:04:05 +0200

    compile.sh updated to work with current binaries; requires manual change.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-g++ -std=c++23 -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization           \
+g++ -std=c++14 -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization           \
        	-Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast \
 	-Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel         \
-	-Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused                                              \
-	-o main src/*[^k].cpp -I include/ -fopenmp
+	-Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused                                              \
+	-O3 -march=native                                                                                             \
+	-o slink Slink/slink.cpp src/CSV.cpp -I include/ -fopenmp
